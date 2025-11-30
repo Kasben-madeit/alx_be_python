9194e377cd84ea0e859e0a8c6e14444e8557a1c9
Kasben-madeit <boforiasare@gmail.com>
2025-11-30 22:29:28 +0000

    Update

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
     def __init__(self,account_balance):
         self.account_balance = float(account_balance) 
+        account_balance = 100.00
 
     def deposit(self,amount):
         self.amount = float(amount)
@@ -11,7 +12,7 @@ class BankAccount:
         self.account_balance -= amount
 
     def display_balance(self):
-        print(f"Current Balance: $ {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
     
 # d1 = BankAccount(500)
 # d1.display_balance()
