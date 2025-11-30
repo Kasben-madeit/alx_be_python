4efc8bdc2705d59a32245aec0ffb9932a5b16161
Kasben-madeit <boforiasare@gmail.com>
2025-11-30 22:02:41 +0000

    bank account task

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.account_balance = float(account_balance) 
+
+    def deposit(self,amount):
+        self.amount = float(amount)
+        self.account_balance += amount
+
+    def withdraw(self,amount):
+        self.amount = float(amount)
+        self.account_balance -= amount
+
+    def display_balance(self):
+        print(f"Current balance:$ {self.account_balance}")
+    
+# d1 = BankAccount(500)
+# d1.display_balance()
+# d1.deposit(200)
+# d1.display_balance()
